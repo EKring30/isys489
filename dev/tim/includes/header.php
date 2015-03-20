ab953bffa0ab5273aedaabaa2ceefb8d0c916426
Tim Hansen <willc0de4food@gmail.com>
2015-03-19 18:50:18 -0700

    made login a dropdown

--- dev/tim/includes/header.php
+++ dev/tim/includes/header.php
@@ -19,7 +19,21 @@
           <ul class="nav navbar-nav navbar-right">
             <?php
               if (empty($_SESSION['user'])) { ?>
-                <li<?php if ($page == "login") {?> class="active"<?php } ?>><a href="login.php">Login</a></li>
+                <li class="dropdown<?php if ($page == "login") {?> active<?php }?>">
+                  <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+                  <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+                    <form method="post" action="login" accept-charset="UTF-8">
+                      <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+                      <input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+                      <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+                      <label class="string optional" for="user_remember_me"> Remember me</label>
+                      <input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
+                      <label style="text-align:center;margin-top:5px">or</label>
+                                      <input class="btn btn-primary btn-block" type="button" id="sign-in-google" value="Sign In with Google">
+                      <input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Sign In with Twitter">
+                    </form>
+                  </div>
+                </li>
               <?php } else { ?>
                 <li><a name="user">Hello, <?php echo $_SESSION["user"]["name"] ?></a></li>
                 <li><a href="logout.php">Logout</a></li>
