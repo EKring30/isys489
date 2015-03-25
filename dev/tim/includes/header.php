94c2efa19a5d3636aec7f95c701ef5dccb289a97
Tim Hansen <willc0de4food@gmail.com>
2015-03-24 17:32:46 -0700

    updated login dropdown. added login code

--- dev/tim/includes/header.php
+++ dev/tim/includes/header.php
@@ -16,28 +16,23 @@
             <li<?php if ($page == "contact") {?> class="active"<?php } ?>><a href="#contact">Contact</a></li>
             <li<?php if ($page == "test") {?> class="active"<?php } ?>><a href="test_form.php">Test Form</a></li>
           </ul>
-          <ul class="nav navbar-nav navbar-right">
+          <ul class="nav pull-right">
             <?php
               if (empty($_SESSION['user'])) { ?>
-                <li class="dropdown<?php if ($page == "login") {?> active<?php }?>">
-                  <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
-                  <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-                    <form method="post" action="login" accept-charset="UTF-8">
-                      <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
-                      <input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
-                      <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
-                      <label class="string optional" for="user_remember_me"> Remember me</label>
-                      <input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
-                      <label style="text-align:center;margin-top:5px">or</label>
-                                      <input class="btn btn-primary btn-block" type="button" id="sign-in-google" value="Sign In with Google">
-                      <input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Sign In with Twitter">
-                    </form>
-                  </div>
-                </li>
-              <?php } else { ?>
-                <li><a name="user">Hello, <?php echo $_SESSION["user"]["name"] ?></a></li>
-                <li><a href="logout.php">Logout</a></li>
-              <?php } ?>
+            <li class="dropdown" id="menuLogin">
+              <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login <b class="caret"></b></a>
+              <div class="dropdown-menu" style="padding:17px;">
+                <form class="form" id="formLogin"> 
+                  <input name="username" id="username" type="text" placeholder="Username"> 
+                  <input name="pwd" id="pwd" type="password" placeholder="Password"><br>
+                  <button type="button" id="btnLogin" class="btn">Login</button>
+                </form>
+              </div>
+            </li>
+            <?php } else { ?>
+              <li><a name="user">Hello, <?php echo $_SESSION["user"]["name"] ?></a></li>
+              <li><a href="logout.php">Logout</a></li>
+            <?php } ?>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
--- dev/tim/login.php
+++ dev/tim/login.php
@@ -1,130 +1,17 @@
 <?php
 	session_start();
-	include('includes/global-vars.php');
-
-	if (!empty($_SESSION["user"]))
+	if (empty($_POST))
 	{
-		$_SESSION["notice"] = "You are already logged in.";
 		header('Location: index.php');
-		break;
 	}
-
-	if (isset($_POST["username"]) && isset($_POST["password"]))
+	else
 	{
-		$_SESSION["user"] = array();
-		$_SESSION["user"]["name"] = $_POST["username"];
-		$_SESSION["notice"] = "Login Successful";
-		header('Location: index.php');
-		break;
+		if (!empty($_POST['username']) && !empty($_POST['pwd']))
+		{
+			$_SESSION['user'] = $_POST['username'];
+			header('Location: index.php');
+		}
+		else
+			echo "Error - username or password invalid.";
 	}
-
-	if ((isset($_POST["username"]) && empty($_POST["password"])) || empty($_POST["username"]) && isset($_POST["password"]))
-	{
-		$_SESSION["error"] = "Error - you must enter both a username and a password.";
-	}
-
-	$title = "Login";
-	$page = "login";
-	$content = "";
-
-	if (isset($_SESSION["notice"]))
-	{
-		$content .= '<div class="alert alert-success" role="alert">'.$_SESSION["notice"].'</div><br /><br />'."\r\n";
-		unset($_SESSION["notice"]);
-	}
-
-	if (isset($_SESSION["error"]))
-	{
-		$content .= '<div class="alert alert-danger" role="alert">'.$_SESSION["error"].'</div><br /><br />'."\r\n";
-		unset($_SESSION["error"]);
-	}
-
-	$content = '
-		<div class="row">
-      <div class="col-sm-6 col-md-4 col-md-offset-4">
-        <div class="account-wall">
-          <div id="my-tab-content" class="tab-content">
-						<div class="tab-pane active" id="login">
-       		    <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                    alt="">
-         			<form class="form-signin" action="login.php" method="post">
-         				<input type="text" class="form-control" placeholder="Username" name="username" required autofocus>
-         				<input type="password" class="form-control" placeholder="Password" name="password" required>
-         				<input type="submit" class="btn btn-lg btn-default btn-block" value="Sign In" />
-         			</form>
-         			<div id="tabs" data-tabs="tabs">
-         				<p class="text-center"><a href="#register" data-toggle="tab">Need an Account?</a></p>
-         				<p class="text-center"><a href="#select" data-toggle="tab">Select Account</a></p>
-      				</div>
-						</div>
-						<div class="tab-pane" id="register">
-							<form class="form-signin" action="" method="">
-								<input type="text" class="form-control" placeholder="User Name ..." required autofocus>
-								<input type="email" class="form-control" placeholder="Emaill Address ..." required>
-								<input type="password" class="form-control" placeholder="Password ..." required>
-								<input type="submit" class="btn btn-lg btn-default btn-block" value="Sign Up" />
-							</form>
-							<div id="tabs" data-tabs="tabs">
-               			<p class="text-center"><a href="#login" data-toggle="tab">Have an Account?</a></p>
-              			</div>
-						</div>
-						<div class="tab-pane" id="select">
-							<div id="tabs" data-tabs="tabs">
-								<div class="media account-select">
-									<a href="#user1" data-toggle="tab">
-										<div class="pull-left">		
-											<img class="select-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                    alt="">
-										</div>	 
-										<div class="media-body">
-											<h4 class="select-name">User Name 1</h4>
-										</div>
-									</a>
-								</div>
-                                <hr />
-								<div class="media account-select">
-									<a href="#user2" data-toggle="tab">
-										<div class="pull-left">		
-											<img class="select-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                    alt="">
-										</div>	 
-										<div class="media-body">
-											<h4 class="select-name">User Name 2</h4>
-										</div>
-									</a>
-								</div>
-                                <hr />
-               			<p class="text-center"><a href="#login" data-toggle="tab">Back to Login</a></p>
-              			</div>
-						</div>
-						<div class="tab-pane" id="user1">
-							<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                    alt="">
-							<h3 class="text-center">User Name 1</h3>
-							<form class="form-signin" action="" method="">
-								<input type="hidden" class="form-control" value="User Name">
-								<input type="password" class="form-control" placeholder="Password" autofocus required>
-								<input type="submit" class="btn btn-lg btn-default btn-block" value="Sign In" />
-							</form>
-							<p class="text-center"><a href="#login" data-toggle="tab">Back to Login</a></p>
-               		<p class="text-center"><a href="#select" data-toggle="tab">Select another Account</a></p>
-						</div>
-						<div class="tab-pane" id="user2">
-							<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                    alt="">
-							<h3 class="text-center">User Name 2</h3>
-							<form class="form-signin" action="" method="">
-								<input type="hidden" class="form-control" value="User Name">
-								<input type="password" class="form-control" placeholder="Password" autofocus required>
-								<input type="submit" class="btn btn-lg btn-default btn-block" value="Sign In" />
-							</form>
-							<p class="text-center"><a href="#login" data-toggle="tab">Back to Login</a></p>
-               		<p class="text-center"><a href="#select" data-toggle="tab">Select another Account</a></p>
-						</div>
-					</div>
-            </div>
-        </div>
-    </div>
-	'."\r\n";
-	include('includes/template.php');
 ?>
