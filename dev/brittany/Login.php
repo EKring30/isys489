1ae9082c0800060657c11507274469cab4a7ced6
gerndtp <gerndtp@ferris.edu>
2015-03-29 17:56:29 -0400

    Fixed login forms
    For Brittany :)

--- dev/brittany/Login.php
+++ dev/brittany/Login.php
@@ -1,11 +1,7 @@
-
-<!DOCTYPE html>
-<html>
-<body>
 <?php
 session_start();
 include('includes/database.php');
-include('includes/header.php');
+//include('includes/header.php');
 global $my_dbhandle;
 
 if($my_dbhandle->connect_error)
@@ -21,26 +17,35 @@ else if(empty($_POST['password']))
     {
         echo "Error- Please enter Password.";
     }
-     
+	
 else
    {
 	   
 	 //look up username and password in db
-	 $stmt = $conn->prepare("SELECT dUsername, dPassword FROM users WHERE username = dUsername AND password = dPassword");
-	 $stmt -> bind_param("ss", $_POST['username'], md5($_POST['password']));
-	 $res = $stmt->get_result();
-	 if (!$res)
+	 if($stmt = $my_dbhandle->prepare("SELECT userName FROM users WHERE email = ? AND password = ?")) 
 	 {
-		 echo "Error- invalid username or password.";
-	 }//end of if
-	 else
-	 {
-	 $_SESSION['username'] = $_POST['username'];
-	 echo "success";
-	 }//end of inner else
-   }//end of else
-    
+		
+		if(isset($_POST['username'])) $u = $_POST['username'];
+		if(isset($_POST['password'])) $p = md5($_POST['password']);
+		
+		$stmt->bind_param("ss", $u, $p);
+		$stmt->execute();
+		
+		/* bind result variables */
+		$stmt->bind_result($res);
 
+		/* fetch value */
+		$stmt->fetch();
+	 }
+	if (!$res)	// Select was executed - no value returned
+	{
+		echo "Error- invalid username or password.";
+		
+	} 		//end of if
+	else	// Successful Login
+	{
+		$_SESSION['username'] = $_POST['username'];
+		printf("Welcome %s, your login is successful",  $res);
+	}	//end of inner else
+   }//end of else
 ?>
-</body>
-</html>
