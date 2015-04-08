e9dcbede6a4dc0c69df0791071471a4f9bf941f0
russeb10 <russeb10@ferris.edu>
2015-04-08 17:28:04 -0400

    Update Service_Form.php

--- Service_Form.php
+++ Service_Form.php
@@ -1,3 +1,25 @@
+<?php
+	//detect form submission
+	if (isset($_POST['submit'])) {
+	//form was submitted
+	$categories = $_POST['categories'];
+	$DescriptionOfServices = $_POST['DescriptionOfServices'];
+	$CostOfService = $_POST['CostOfService'];
+	$PaymentOfJob = $_POST['PaymentOfJob'];
+	$Supplies =$_POST['SuppliesProvided'];
+	$AvailableTimes = $_POST['AvailableTimes'];
+	$TransportationProvided = $_POST['TransportationProvided'];
+	echo "Form was submitted";
+	//successful login
+	 redirect_to('database.php');
+	}else{
+	$message = "There were some errors";
+?>
+//validation by Tim
+   <?php
+      include('includes/validations.php');
+    ?>
+	
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -14,9 +36,8 @@ body,td,th {
 
 <body>
 
-include('includes/validations.php');
-
  <form name="my_form" action="process_form.php" method="post">
+
   <fieldset>
   <table width="1086" height="26" border="0">
   <caption align="left"><span class="style2">Provider-Consumer Jobs</span>
@@ -24,7 +45,7 @@ include('includes/validations.php');
   
   <tr>
     <th width="99" scope="col"><div align="left">Categories</div></th>
-    <th width="977" scope="col">Search</th>
+    
   </tr>
 </table>
 
@@ -90,12 +111,16 @@ Payment for job<br />
   </p>
   <br />
   <br />
- 
+
+ <?php
+   $todaysDate = date("m/d/y");
+     echo "Today is:";
+     echo $todaysDate;
+?>
   <span class="style2">Available Times and Dates</span><br />
     <textarea cols="40" rows="3" name=""> </textarea>
 	<br />
 	<br />
-	
 	
 	<span class="style3">Transportation Provided</span><br />
 	<input name="Yes" type="radio" value="Yes" />Yes
@@ -106,3 +131,6 @@ Payment for job<br />
 
 </body>
 </html>
+
+
+
